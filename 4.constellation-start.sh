387b9b92ad3a52107247f5cbdfdc4b6089d99e62
po <po@sqlpanda.com>
2018-06-19 02:27:01 +0000

    Start with $HOME direcory

--- 4.constellation-start.sh
+++ 4.constellation-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -u
 set -e
-
+cd $HOME
 echo "[*] Starting Constellation node"
 touch qdata/logs/constellation.log
 nohup constellation-node raft/tm.conf 2>> qdata/logs/constellation.log &
