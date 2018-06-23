dea1eece3076be7107ff25c1d16e86535d391a68
Ubuntu <ubuntu@ip-172-30-0-76.ec2.internal>
2018-06-23 05:58:02 +0000

    add more parameter

--- 5.raft-start.sh
+++ 5.raft-start.sh
@@ -2,7 +2,7 @@
 set -u
 set -e
 cd $HOME
-GLOBAL_ARGS="--raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum"
+GLOBAL_ARGS="--nodiscover --verbosity 6 --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum"
 
 # echo "[*] Starting Constellation node"
 # nohup constellation-node tm.conf 2>> qdata/logs/constellation.log &
@@ -11,4 +11,4 @@ GLOBAL_ARGS="--raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,ne
 
 
 echo "[*] Starting geth node"
-PRIVATE_CONFIG=raft/tm.conf nohup geth --datadir qdata $GLOBAL_ARGS --rpccorsdomain "*" --rpcport 21001 --port 21002 --raftport 21003 --unlock 0 --password raft/passwords.txt 2>>qdata/logs/geth.log &
+PRIVATE_CONFIG=raft/tm.conf nohup geth --datadir qdata $GLOBAL_ARGS --networkid 8185 --rpccorsdomain "*" --rpcport 21001 --port 21002 --raftport 21003 --unlock 0 --password raft/passwords.txt 2>>qdata/logs/geth.log &
